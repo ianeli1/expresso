7dda597e5735c2242f12abc7760b406fc1eb33ab
ianeli1 <ianelicha@gmail.com>
2021-04-20 21:26:35 -0600

    fetch polyfill thing

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
 export { Expresso } from "./Expresso";
 export { Router } from "./Router";
+import https from "http";
+
+interface FetchResult {
+  json(): any;
+  data: string;
+}
+
+//polyfill for fetch kinda
+export function fetch(url: string) {
+  return new Promise<FetchResult>((res, rej) => {
+    https.get(url, (response) => {
+      response.on("error", rej);
+      let body = "";
+      response.on("data", (e) => (body += e));
+      response.on("end", () => {
+        res({
+          json() {
+            return JSON.parse(body);
+          },
+          data: body,
+        });
+      });
+    });
+  });
+}
